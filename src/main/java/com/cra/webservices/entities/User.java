13a4046e33ac6c742c647f466737952bddc4f1ae
Celso Aquino <celso.aquino@ymail.com>
2020-01-30 14:38:06 -0300

    H2 database, test profile, JPA

--- src/main/java/com/cra/webservices/entities/User.java
+++ src/main/java/com/cra/webservices/entities/User.java
@@ -1,19 +1,22 @@
 package com.cra.webservices.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 @AllArgsConstructor
 @Getter @Setter
+@NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User implements Serializable {
 
     private static final long serialVersionUID = 5135749675626057511L;
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
     private String name;
