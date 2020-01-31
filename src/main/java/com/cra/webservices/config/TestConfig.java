c865675ca23bc0816ad1440a1594aabf488c9205
Celso Aquino <celso.aquino@ymail.com>
2020-01-31 01:17:28 -0300

    OrderStatus enum

--- src/main/java/com/cra/webservices/config/TestConfig.java
+++ src/main/java/com/cra/webservices/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.cra.webservices.config;
 
 import com.cra.webservices.entities.Order;
 import com.cra.webservices.entities.User;
+import com.cra.webservices.entities.enums.OrderStatus;
 import com.cra.webservices.repositories.OrderRepository;
 import com.cra.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
        User u1 = new User(null, "Maria brown", "maria@gmail.com","3535-5454","12345");
        User u2 = new User(null, "Alex green", "alex@gmail.com","5454-3535","54321");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),  OrderStatus.DELIVERED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),  OrderStatus.CANCELED, u1);
 
 
         userRepository.saveAll(Arrays.asList(u1,u2));
--- src/main/java/com/cra/webservices/entities/Order.java
+++ src/main/java/com/cra/webservices/entities/Order.java
@@ -1,5 +1,6 @@
 package com.cra.webservices.entities;
 
+import com.cra.webservices.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,7 +13,6 @@ import java.time.Instant;
 
 @Entity
 @Table(name = "TB_ORDER")
-@Getter @Setter
 public class Order implements Serializable {
 
     private static final long serialVersionUID = -5223250568916149836L;
@@ -25,6 +25,9 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-mm-dd'T'HH:mm:s'Z'", timezone = "GMT")
     private Instant instant;
 
+
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -32,10 +35,45 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant instant, User client){
+    public Order(Long id, Instant instant, OrderStatus orderStatus, User client ){
         super();
         this.id = id;
         this.instant = instant;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+
+    public void setInstant(Instant instant) {
+        this.instant = instant;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
         this.client = client;
     }
 }
